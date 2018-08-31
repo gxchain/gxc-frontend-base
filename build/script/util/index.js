bd3e94782df9b05fdf0f4da3cf98f2e9747faf92
刘宗源 <940101379@qq.com>
2018-08-31 10:46:05 +0800

    chore: build

--- build/script/util/index.js
+++ build/script/util/index.js
@@ -3,6 +3,10 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+exports.utf8_to_b64 = utf8_to_b64;
+exports.b64_to_utf8 = b64_to_utf8;
+var $client = undefined || {};
+
 /**
  * multiply, no overflow
  * @param {Number} arg1 
@@ -20,3 +24,27 @@ var accMult = exports.accMult = function accMult(arg1, arg2) {
     } catch (e) {}
     return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m);
 };
+
+/**
+ * utf8_to_b64
+ * @param {String} str 
+ */
+function utf8_to_b64(str) {
+    if ($client.btoa) {
+        return $client.btoa(unescape(encodeURIComponent(str)));
+    } else {
+        return new Buffer(str).toString('base64');
+    }
+}
+
+/**
+ * b64_to_utf8
+ * @param {String} str 
+ */
+function b64_to_utf8(str) {
+    if ($client.atob) {
+        return decodeURIComponent(escape($client.atob(str)));
+    } else {
+        return new Buffer(str, 'base64').toString();
+    }
+}
