7a69191b83b98f0678b8d9905dfc742f55d91ce2
刘宗源 <940101379@qq.com>
2018-08-31 10:44:36 +0800

    feat: add utf8_to_b64 and reverse

--- test/script/util/index.spec.js
+++ test/script/util/index.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import {accMult} from '../../../src/script/util/index'
+import {accMult, utf8_to_b64, b64_to_utf8} from '../../../src/script/util/index'
 
 describe("accMult", () => {
   before(() => {
@@ -17,3 +17,29 @@ describe("accMult", () => {
   })
 
 })
+
+describe("utf8_to_b64", () => {
+    before(() => {
+    })
+  
+    after(() => {
+    })
+  
+    it("case1", function() {
+      expect(utf8_to_b64('我喜欢你')).to.equal('5oiR5Zac5qyi5L2g')
+    })
+  
+  })
+
+  describe("b64_to_utf8", () => {
+    before(() => {
+    })
+  
+    after(() => {
+    })
+  
+    it("case1", function() {
+      expect(b64_to_utf8('5oiR5Zac5qyi5L2g')).to.equal('我喜欢你')
+    })
+  
+  })
