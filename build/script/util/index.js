54382db388e32824aa4d3dc11690829d7f8a8d9d
刘宗源 <940101379@qq.com>
2018-12-17 20:04:42 +0800

    chore: add validate_account_name

--- build/script/util/index.js
+++ build/script/util/index.js
@@ -5,12 +5,13 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.utf8_to_b64 = utf8_to_b64;
 exports.b64_to_utf8 = b64_to_utf8;
+exports.validate_account_name = validate_account_name;
 var $client = undefined || {};
 
 /**
  * multiply, no overflow
- * @param {Number} arg1 
- * @param {Number} arg2 
+ * @param {Number} arg1
+ * @param {Number} arg2
  */
 var accMult = exports.accMult = function accMult(arg1, arg2) {
     var m = 0;
@@ -27,7 +28,7 @@ var accMult = exports.accMult = function accMult(arg1, arg2) {
 
 /**
  * utf8_to_b64
- * @param {String} str 
+ * @param {String} str
  */
 function utf8_to_b64(str) {
     if ($client.btoa) {
@@ -39,7 +40,7 @@ function utf8_to_b64(str) {
 
 /**
  * b64_to_utf8
- * @param {String} str 
+ * @param {String} str
  */
 function b64_to_utf8(str) {
     if ($client.atob) {
@@ -47,4 +48,48 @@ function b64_to_utf8(str) {
     } else {
         return new Buffer(str, 'base64').toString();
     }
+}
+
+/**
+ * validate gxchain account name
+ * @param {String} name 
+ */
+function validate_account_name(name) {
+    var i, label, len, length;
+
+    if (!name) {
+        return 'empty_name';
+    }
+    length = name.length;
+    if (length < 3) {
+        return 'name_should_be_longer';
+    }
+    if (length > 63) {
+        return 'name_should_be_shorter';
+    }
+
+    if (!(/[0-9-]/.test(name) || !/[aeiouy]/.test(name))) {
+        return 'premium_name';
+    }
+
+    var ref = name.split('.');
+    for (i = 0, len = ref.length; i < len; i++) {
+        label = ref[i];
+        if (!/^[~a-z]/.test(label)) {
+            return 'name_should_start_with_a_letter';
+        }
+        if (!/^[~a-z0-9-]*$/.test(label)) {
+            return 'name_format_error';
+        }
+        if (/--/.test(label)) {
+            return 'name_one_dash_error';
+        }
+        if (!/[a-z0-9]$/.test(label)) {
+            return 'name_end_error';
+        }
+        if (!(label.length >= 3)) {
+            return 'name_segment_should_be_longer';
+        }
+    }
+    return null;
 }
--- src/script/util/index.js
+++ src/script/util/index.js
@@ -2,8 +2,8 @@ const $client = this || {}
 
 /**
  * multiply, no overflow
- * @param {Number} arg1 
- * @param {Number} arg2 
+ * @param {Number} arg1
+ * @param {Number} arg2
  */
 export const accMult = (arg1, arg2) => {
     let m = 0
@@ -22,24 +22,68 @@ export const accMult = (arg1, arg2) => {
 
 /**
  * utf8_to_b64
- * @param {String} str 
+ * @param {String} str
  */
 export function utf8_to_b64(str) {
-    if($client.btoa){
+    if ($client.btoa) {
         return $client.btoa(unescape(encodeURIComponent(str)))
-    }else{
+    } else {
         return new Buffer(str).toString('base64')
     }
 }
 
 /**
  * b64_to_utf8
- * @param {String} str 
+ * @param {String} str
  */
 export function b64_to_utf8(str) {
-    if($client.atob){
+    if ($client.atob) {
         return decodeURIComponent(escape($client.atob(str)))
-    }else{
+    } else {
         return new Buffer(str, 'base64').toString()
     }
+}
+
+/**
+ * validate gxchain account name
+ * @param {String} name 
+ */
+export function validate_account_name(name) {
+    var i, label, len, length
+
+    if (!name) {
+        return 'empty_name'
+    }
+    length = name.length
+    if (length < 3) {
+        return 'name_should_be_longer'
+    }
+    if (length > 63) {
+        return 'name_should_be_shorter'
+    }
+
+    if (!(/[0-9-]/.test(name) || !/[aeiouy]/.test(name))) {
+        return 'premium_name'
+    }
+
+    let ref = name.split('.')
+    for (i = 0, len = ref.length; i < len; i++) {
+        label = ref[i]
+        if (!/^[~a-z]/.test(label)) {
+            return 'name_should_start_with_a_letter'
+        }
+        if (!/^[~a-z0-9-]*$/.test(label)) {
+            return 'name_format_error'
+        }
+        if (/--/.test(label)) {
+            return 'name_one_dash_error'
+        }
+        if (!/[a-z0-9]$/.test(label)) {
+            return 'name_end_error'
+        }
+        if (!(label.length >= 3)) {
+            return 'name_segment_should_be_longer'
+        }
+    }
+    return null
 }
